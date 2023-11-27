aa62e79740ad62c356a71604eb99a1c791751f2b
Olivier Agbala <ronniking99@gmail.com>
2023-11-27 09:46:09 +0100

    close modale

---
+++ starterOnly/js/form.js
@@ -0,0 +1,16 @@
+// DOM Elements
+
+
+const formData = document.querySelectorAll(".formData");
+
+//fonction point d'entrée de l'application
+function main() {
+    init_event();
+}
+
+//Initialisation des événements du formulaire
+function init_event() {
+
+}
+
+main();
---
+++ starterOnly/js/modal.js
@@ -0,0 +1,46 @@
+function editNav() {
+	var x = document.getElementById("myTopnav");
+	if (x.className === "topnav") {
+		x.className += " responsive";
+	} else {
+		x.className = "topnav";
+	}
+}
+
+
+
+
+// launch modal form
+function launchModal() {
+	const modalbg = document.querySelector(".bground");
+	modalbg.style.display = "block";
+}
+
+// close modal form
+function closeModal() {
+	const modalbg = document.querySelector(".bground");
+	modalbg.style.display = "none";
+}
+
+//fonction point d'entrée de l'application
+function main() {
+	init_event();
+}
+
+//Initialisation des événements de la modal
+function init_event() {
+	const modalBtn = document.querySelectorAll(".modal-btn");
+	const modalCloseBtn = document.querySelectorAll(".close");
+
+	// launch modal event
+	modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
+	// close modal event
+	modalCloseBtn.forEach((btn) => btn.addEventListener("click", closeModal));
+
+}
+
+main();
+
+
+
